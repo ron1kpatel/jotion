fd3a3cd39055a4561bc570e4fb07c78c8f6fb0bd
ron1kpatel <rp140267@gmail.com>
2024-06-25 12:57:52 +0530

    Base system is ready

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,8 @@
 export default {
-    providers: [
-      {
-        domain: "https://awake-ox-31.clerk.accounts.dev",
-        applicationID: "convex",
-      }
-    ]
-  }
+  providers: [
+    {
+      domain: "https://ethical-wildcat-51.clerk.accounts.dev",
+      applicationID: "convex"
+    }
+  ]
+}
